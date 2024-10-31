0b658397d1059fc4b46bf9fd4e32ee3ef8193ac2
jasonberba-vls <127728314+jasonberba-vls@users.noreply.github.com>
2024-10-31 16:07:04 +0400

    Update

--- src/kafka-producer/partitioner/merchant-partitioner.ts
+++ src/kafka-producer/partitioner/merchant-partitioner.ts
@@ -19,21 +19,22 @@ function merchantPartitioner(kafkaMessage: any): number {
     // console.log('merchantPartitioner kafkaMessage.message : ', message);
     // console.log('merchantPartitioner partitionCount : ', partitionCount);
 
+    // Round Robin Logic - Initialize Round Robin Partition Value
+    returnValue = Math.floor(Math.random() * partitionCount);
+    //console.log('returnValue', returnValue)
+
     // Logic will be based on merchantId being passed as the message Key
     if (message.key) {
         //console.log('merchantPartitioner partitionMapping', partitionMapping);
 
         // Find the appropriate partition based on the merchantId
         for (const mapping of partitionMapping.PartitionMapping) {
-            if (mapping.MerchantIdList.includes(message.key)) {
-                return mapping.Partition; // Return the corresponding partition
+            if (mapping.MerchantIdList.includes(Number(message.key))) {
+                returnValue = mapping.Partition; // Return the corresponding partition
             }
         }
+        return returnValue;
     }
-
-    // Round Robin Logic
-    returnValue = Math.floor(Math.random() * partitionCount);;
-    // console.log('returnValue', returnValue)
     return returnValue;
 };
 
